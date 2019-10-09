2b27dedd4fd1390c0e5db04a057576a19c598388
Jonathan <jote962010@hotmail.com>
2019-10-09 18:12:05 +0200

    Begin on a raycast manager

---
+++ Swinger Slinger/Assets/Scripts/Managers/RaycastManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+    public Camera playerCamera;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(transform.position, Vector3.forward, Color.blue, 2);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                hit.transform.gameObject.GetComponent<Rigidbody>().velocity += new Vector3(0,3,0);
+                Debug.Log(hit.transform.gameObject);
+            }
+        }
+    }
+}
--- Swinger Slinger/Assets/Scripts/PlayerScripts/InputManager.cs
+++ Swinger Slinger/Assets/Scripts/PlayerScripts/InputManager.cs
@@ -14,37 +14,37 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            playerRb.velocity = new Vector3(0,0,1);
+            playerRb.velocity = new Vector3(0,0,2);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            playerRb.velocity = new Vector3(-1, 0, 0);
+            playerRb.velocity = new Vector3(-2, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            playerRb.velocity = new Vector3(1, 0, 0);
+            playerRb.velocity = new Vector3(2, 0, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            playerRb.velocity = new Vector3(0, 0, -1);
+            playerRb.velocity = new Vector3(0, 0, -2);
         }
 
 
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
-            playerRb.velocity = new Vector3(1, 0, 1);
+            playerRb.velocity = new Vector3(2, 0, 2);
         }
         if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
         {
-            playerRb.velocity = new Vector3(1, 0, -1);
+            playerRb.velocity = new Vector3(2, 0, -2);
         }
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
         {
-            playerRb.velocity = new Vector3(-1, 0, -1);
+            playerRb.velocity = new Vector3(-2, 0, -2);
         }
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
         {
-            playerRb.velocity = new Vector3(-1, 0, 1);
+            playerRb.velocity = new Vector3(-2, 0, 2);
         }
     }
 }
